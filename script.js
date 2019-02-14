486ff7d67852e9f137b034aa1edd3ca53c296eab
Seth Mcbay <sethmcbay@yahoo.com>
2019-02-14 16:55:10 -0500

    added click event on start button

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-
 let arrOfFruits = ['Apple', 'Orange', 'Pear', 'Banana', 'Pineapple']
+// let arrOfLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] 
 
-getRandomNum = (apple) => {
-  return Math.floor(Math.random() * apple.length)
-}
-console.log(arrOfFruits[getRandomNum(arrOfFruits)])
+
+$('#start').on('click', getRandomWord = () => {
+    let ans = Math.floor(Math.random() * arrOfFruits.length)
+    console.log(arrOfFruits[ans])
+    return arrOfFruits[ans]
+})
+
+
+
+// console.log(arrOfFruits[getRandomWord(arrOfFruits)])
+
+
