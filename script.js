599048edc988849e4f63e04316201b1af88d2fcc
Seth Mcbay <sethmcbay@yahoo.com>
2019-02-15 16:57:41 -0500

    added funcuality to my game

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let arrOfFruits = ['APPLE', 'ORANGE', 'PEAR', 'BANANA', 'PINEAPPLE']
 let word = ''
+let counter = 0
+let score = 0
+let limit = 6
 
+function userScores() {
+  counter += 1
+  $('.scoreboard').html(counter)
+  console.log(counter)
+}
 
 $('#start').on('click', getRandomWord = () => {
   let ans = Math.floor(Math.random() * arrOfFruits.length)
@@ -14,17 +22,47 @@ $('#start').on('click', getRandomWord = () => {
 resetFunc = () => {
   document.location.reload()
 }
+let loseCounter = 6
 
-$('.grid-item').on('click', function(){
+$('.grid-item').on('click', function () {
   $(this).css('color', 'lightgray')
   let letterThis = $(this)[0].textContent
   console.log(letterThis)
   $(`.${letterThis}`).text(letterThis)
+  checkForWin(letterThis)
 })
-  
-  
+
+function checkForWin(letter) {
+  let amountOfTimesLetterIsNotInWord = 0
+  for (let i = 0; i < word.length; i++) {
+    if (letter !== word[i]) {
+      amountOfTimesLetterIsNotInWord += 1
+      if (amountOfTimesLetterIsNotInWord === word.length) {
+        limit -= 1
+        console.log(limit)
+      }
+    } else {
+      score += 1
+      console.log(score)
+    }
+   
+    if (limit === 0)
+      alert ('you lose')
+
+  }
+
+}
+
+
 
 // arrayOfFruits.indexOf(event.currentTarget.innerHTML)
 
 
 // console.log(arrOfFruits[getRandomWord(arrOfFruits)])
+
+// SCOREBOARD
+// create a counter variable set to 0 by default
+// when a player scores, increase the counter variable by 1
+// then use the html() method to display the score in the browser
+// for example
+
